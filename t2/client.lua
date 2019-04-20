c1ce392b8c87fc547f7b4631262d76ff02b877d1
Thiago Naves <naves.thiago@gmail.com>
2019-04-19 21:43:30 -0300

    Added response handling to the method wrapper

--- t2/client.lua
+++ t2/client.lua
@@ -43,7 +43,7 @@ function struct(t)
 	if not validade_struct(t) then
 		error('Invalid struct')
 	end
-    _structs[t.name] = t.fields
+	_structs[t.name] = t.fields
 end
 
 function interface(t)
@@ -62,12 +62,24 @@ function interface(t)
 		end
 
 		_ENV[name] = function (self, ...)
+			local function show_error(msg)
+				error(name .. ': ' .. msg, 3)
+			end
 			local name, exp_args, results = name, params, results
 			local args = {...}
 			local ok, err = validate_args(args, exp_args)
-			if not ok then error(name .. ': ' .. err, 2) end
+			if not ok then show_error(err) end
 			ok, err = self.socket:send(mime.b64(binser.serialize(name, ...)) .. '\n')
-			if not ok then error(name .. ': ' .. err, 2) end
+			if not ok then show_error(err) end
+
+			if #results > 1 or results[1] ~= 'void' then
+				local response, err = self.socket:receive('*l')
+				if err then show_error(err) end
+				response = mime.unb64(response)
+				local ok, data = pcall(binser.deserialize(response))
+				if not ok then show_error(data) end
+				return table.unpack(data, 1, #results)
+			end
 		end
 	end
 end
