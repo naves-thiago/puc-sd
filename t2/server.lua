3f43603a2bc703e76a9a025162b6d684ef89e145
Victor Nogueira <victor.nogueira@me.com>
2019-04-21 18:35:26 -0300

    add connection pool to server

--- t2/server.lua
+++ t2/server.lua
@@ -1,8 +1,8 @@
 local socket = require'socket'
 local binser = require'binser'
 local mime = require'mime'
+local types = require'types'
 
-local idl_file = 'idl1.lua'
 idls = {}
 
 idls.interfaces = {}
@@ -13,48 +13,15 @@ local unconnected = {}
 local connected = {}
 local sockets = {}
 
-local type_translate = {
-    string = 'string',
-    int = 'number',
-    double = 'number',
-    void = 'nil',
-}
-
-local function validate_primitive_res(res, args, structinfo)
-    for _, arg in ipairs(args) do
-        if arg.type ~= 'double' and arg.type ~= 'int' and arg.type ~= 'string' and arg.type ~= 'void' then
-            local fields
-            for _, struct in pairs(structinfo) do
-               if struct.name== arg.type  then
-                   fields = struct.fields
-               end
+function validate_recv(recv, expected)
+        for i, received in ipairs(recv) do
+            local ok, err = types.validate_type(received, expected[i].type)
+            if not ok then
+                return ok, err
             end
-            if not fields then return end
-            return validate_primitive_res(res, fields, structinfo)
-        else
-            if type_translate[arg.type] ~= type(res[1]) and arg.type ~= 'void' then return end
-            table.remove(res, 1)
         end
-    end
-    return true
+        return true
 end
-
-local function validate_res(results, args, res_type, structinfo)
-    if #results == 0 and #args == 1 and res_type == 'void' then return true end
-    local inout_args = {}
-    inout_args[1] = {type = res_type}
-    for _, arg in ipairs(args) do
-        if arg.direction == 'inout' then
-            table.insert(inout_args, arg)
-        end
-    end
-    return validate_primitive_res(results, inout_args, structinfo)
-end
-
-function validate_args(recv, expected, structinfo)
-	return validate_primitive_res(recv, expected, structinfo)
-end
-
 
 function validade_struct(t)
 	if type(t.name) ~= 'string' then return end
@@ -105,9 +72,7 @@ local function parse_incoming(incoming, idl)
     local name = received[1]
     table.remove(received, 1)
     local methinfo = idl.interface.methods[name]
-    local aux_received = {}
-    table.move(received, 1, #received, 1, aux_received)
-    validate_args(aux_received, methinfo.args, idl.structs)
+    print('args', validate_recv(received, methinfo.args))
     local res = table.pack(pcall(methinfo.func, table.unpack(received)))
     return res, name
 end
@@ -118,9 +83,10 @@ local function parse_and_check_results(res, idl, name)
     end
     table.remove(res, 1)
     local methinfo = idl.interface.methods[name]
-    local aux_res = {}
-    table.move(res, 1, #res, 1, aux_res)
-    local veridict = validate_res(aux_res, methinfo.args, methinfo.resulttype, idl.structs)
+    local exp_results = {{type = methinfo.resulttype}}
+    table.move(methinfo.args, 1, #methinfo.args, 2, exp_results)
+    local veridict, err = validate_recv(res, exp_results)
+    print('rets', veridict, err)
     return veridict and res or false, 'wrong return types'
 end
 
@@ -150,20 +116,28 @@ local function waitincoming()
                         sock:settimeout(1)
                         local client = sock:accept()
                         if client then
+                            if #connected == 3 then
+                                local sock_toclose = next(connected, nil)
+                                connected[sock_toclose] = nil
+                                sock_toclose:close()
+                            end
                             connected[client] = unconnected[sock]
                             table.insert(sockets, client)
                         end
                     else
                         sock:settimeout(1)
                         local line, err = sock:receive('*l')
-                        local res, name = parse_incoming(line, connected[sock])
-                        res, err = parse_and_check_results(res, connected[sock], name)
-                        if res then
-                            sock:send(mime.b64(binser.serialize(table.unpack(res))) .. '\n')
+                        if line then
+                            local res, name = parse_incoming(line, connected[sock])
+                            res, err = parse_and_check_results(res, connected[sock], name)
+                            if res then
+                                sock:send(mime.b64(binser.serialize(table.unpack(res))) .. '\n')
+                            else
+                                sock:send(mime.b64('__RPC_ERROR ' .. err).. '\n')
+                            end
                         else
-                            sock:send(mime.b64('__RPC_ERROR ' .. err).. '\n')
+                                sock:send(mime.b64('__RPC_ERROR couldn\'t receive payload').. '\n')
                         end
-                        --sock:close()
                     end
                 end
             end
