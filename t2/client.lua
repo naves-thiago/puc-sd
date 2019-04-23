b38d51e0bb9fca729fcdec05ebaca1cc8c790aaf
Thiago Naves <naves.thiago@gmail.com>
2019-04-22 22:19:28 -0300

    Made client into a module

--- t2/client.lua
+++ t2/client.lua
@@ -1,13 +1,11 @@
-types = require'types'
-binser = require'binser'
-socket = require'socket'
-mime = require'mime'
+local types = require'types'
+local binser = require'binser'
+local socket = require'socket'
+local mime = require'mime'
 
-idl_file = 'idl1.lua'
-interfaces = {}
-structs = {}
+local m = {}
 
-function validate_args(recv, expected)
+local function validate_args(recv, expected, structs)
 	if #recv ~= #expected then
 		return false, 'Expected ' .. #expected .. ' args, got ' .. #recv
 	end
@@ -22,32 +20,13 @@ function validate_args(recv, expected)
 	return true
 end
 
-function struct(t)
-	if not types.validate_struct(t) then
-		error('Invalid struct')
-	end
-	structs[t.name] = t.fields
-	for k, v in pairs(t.fields) do
-		if structs[v.type] then
-			v.type = structs[v.type]
-		end
-	end
-end
-
-function interface(t)
-	if not types.validate_interface(t) then
-		error('Invalid interface')
-	end
-	interfaces[t.name] = t.methods
-end
-
-loadfile(idl_file)()
-function create_proxy(hostname, port, interface)
+local function create_proxy(self, hostname, port, interface)
 	local proxy = {_interface_name = interface}
-	for name, def in pairs(interfaces[interface]) do
+	local _structs = self._structs
+	for name, def in pairs(self._interfaces[interface]) do
 		local params, results = {}, {def.resulttype}
 		for i, arg in ipairs(def.args) do
-			local arg_type = structs[arg.type] or arg.type
+			local arg_type = _structs[arg.type] or arg.type
 			if arg.direction == 'in' or arg.direction == 'inout' then
 				table.insert(params, arg_type)
 			end
@@ -62,7 +41,7 @@ function create_proxy(hostname, port, interface)
 			end
 			local name, exp_args, results = name, params, results
 			local args = {...}
-			local ok, err = validate_args(args, exp_args)
+			local ok, err = validate_args(args, exp_args, _structs)
 			if not ok then show_error(err) end
 			ok, err = self._socket:send(mime.b64(binser.serialize(name, ...)) .. '\n')
 			if not ok then show_error(err) end
@@ -74,7 +53,7 @@ function create_proxy(hostname, port, interface)
 			if not ok then show_error(data) end
 			if data[1] then
 				table.remove(data, 1)
-				local ok, err = validate_args(data, results)
+				local ok, err = validate_args(data, results, _structs)
 				if not ok then
 					show_error(err)
 				end
@@ -89,3 +68,34 @@ function create_proxy(hostname, port, interface)
 	assert(proxy._socket:connect(hostname, port))
 	return proxy
 end
+
+local function init(self, idl_file)
+	_ENV.interfaces = {}
+	_ENV.structs = {}
+	function _ENV.struct(t)
+		if not types.validate_struct(t) then
+			error('Invalid struct')
+		end
+		structs[t.name] = t.fields
+		for k, v in pairs(t.fields) do
+			if structs[v.type] then
+				v.type = structs[v.type]
+			end
+		end
+	end
+
+	function _ENV.interface(t)
+		if not types.validate_interface(t) then
+			error('Invalid interface')
+		end
+		interfaces[t.name] = t.methods
+	end
+
+	loadfile(idl_file)()
+	return {_interfaces = interfaces, _structs = structs, create_proxy = create_proxy}
+end
+setmetatable(m, {__call = init})
+
+
+
+return m
