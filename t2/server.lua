96f8c110934a798ebee946f1cb504e52c2238d35
Thiago Naves <naves.thiago@gmail.com>
2019-04-22 23:19:29 -0300

    Refactor server.lua

--- t2/server.lua
+++ t2/server.lua
@@ -21,46 +21,43 @@ local function validate_recv(recv, expected, structs)
         return true
 end
 
-local function parse_incoming(incoming, idl)
+local function parse_incoming(incoming, idl, interface_name)
     local data = mime.unb64(incoming)
     local received = binser.deserialize(data)
-    local name = received[1]
+    local func_name = received[1]
     table.remove(received, 1)
-    local methinfo = idl.interfaces.methods[name]
+    local methinfo = idl.interfaces[interface_name].methods[func_name]
     local ok, err = validate_recv(received, methinfo.args, idl.structs)
     if not ok then return ok, err end
     local res = table.pack(pcall(methinfo.func, table.unpack(received)))
-    return res, name
+    return res, func_name
 end
 
-local function parse_and_check_results(res, idl, name)
+local function parse_and_check_results(res, idl, interface_name, func_name)
     if not res[1] then
         return false, res[2]
     end
     table.remove(res, 1)
-    local methinfo = idl.interfaces.methods[name]
+    local methinfo = idl.interfaces[interface_name].methods[func_name]
     local exp_results = {{type = methinfo.resulttype}}
     table.move(methinfo.args, 1, #methinfo.args, 2, exp_results)
     local veridict, err = validate_recv(res, exp_results, idl.structs)
     return veridict and res or false, err
 end
 
-function m.register_servant(idlname, o)
-	idls = {}
-	current_idl = idlname
-    idls[idlname] = {}
-	idls[idlname].interfaces = {}
-	idls[idlname].structs = {}
+function m.register_servant(idl_name, interface_name, o)
+	local idl = {}
+	idl.interfaces = {}
+	idl.structs = {}
 
 	function _ENV.struct(t)
 		if not types.validate_struct(t) then
 			error('Invalid struct')
 		end
-		print(idls)
-		idls[current_idl].structs[t.name] = t.fields
+		idl.structs[t.name] = t.fields
 		for k, v in pairs(t.fields) do
-			if idls[current_idl].structs[v.type] then
-				v.type = idls[current_idl].structs[v.type]
+			if idl.structs[v.type] then
+				v.type = idl.structs[v.type]
 			end
 		end
 	end
@@ -69,23 +66,24 @@ function m.register_servant(idlname, o)
 		if not types.validate_interface(t) then
 			error('Invalid interface')
 		end
-		idls[current_idl].interfaces.methods = t.methods
+		idl.interfaces[t.name] = {}
+		idl.interfaces[t.name].methods = t.methods
 	end
 
-	loadfile(idlname, 't')()
-	local idl = idls[idlname]
+	loadfile(idl_name, 't')()
 	local server = assert(socket.bind('*', 0))
 	local ip, port = server:getsockname()
 	for name, func in pairs(o) do
-		idl.interfaces.methods[name].func = func
+		idl.interfaces[interface_name].methods[name].func = func
 	end
-	unconnected[server] = idl
+	unconnected[server] = { _idl = idl, _interface = interface_name }
 	table.insert(sockets, server)
 	return ip, port
 end
 
 function m.waitincoming()
 	while true do
+		local connected_size = 0
 		local socks = socket.select(sockets)
 		for _, sock in pairs(socks) do
 			if type(sock) == 'userdata' then
@@ -93,12 +91,13 @@ function m.waitincoming()
 				if unconnected[sock] then
 					local client = sock:accept()
 					if client then
-						local connected_size = 0
 						for _ in pairs(connected) do
 							connected_size = connected_size + 1
 						end
+						print('mopa', connected_size)
 						if connected_size == 3 then
 							local sock_toclose = next(connected, nil)
+							print('saporra', sock_toclose)
 							connected[sock_toclose] = nil
 							sock_toclose:close()
 						end
@@ -108,13 +107,15 @@ function m.waitincoming()
 				elseif connected[sock] then
 					local line, err = sock:receive('*l')
 					if line then
-						local res, name = parse_incoming(line, connected[sock])
+						local res, func_name = parse_incoming(line, connected[sock]._idl,
+														connected[sock]._interface)
 						if not res then
-							local err = name
+							local err = func_name
 							sock:send(mime.b64(binser.serialize(false, err)).. '\n')
 							goto continue
 						end
-						local res, err = parse_and_check_results(res, connected[sock], name)
+						local res, err = parse_and_check_results(res, connected[sock]._idl,
+														connected[sock]._interface, func_name)
 						if res then
 							sock:send(mime.b64(binser.serialize(true, table.unpack(res))) .. '\n')
 						else
